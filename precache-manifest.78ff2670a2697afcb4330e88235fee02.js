7d8d9e73428c5d1cc02dc81e89cc5fd8a2023c51
guijiangheng <kakaxizhh@gmail.com>
2019-05-22 07:19:35 +0800

    Updates

---
+++ precache-manifest.78ff2670a2697afcb4330e88235fee02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7ab4bd8a4476a75aa2d5e86ff4e19e5",
+    "url": "/app-list/index.html"
+  },
+  {
+    "revision": "3437347248e6c95abf77",
+    "url": "/app-list/static/css/main.2b383aad.chunk.css"
+  },
+  {
+    "revision": "1804701a8d387135431d",
+    "url": "/app-list/static/js/2.52f4c25f.chunk.js"
+  },
+  {
+    "revision": "3437347248e6c95abf77",
+    "url": "/app-list/static/js/main.dad5cfdd.chunk.js"
+  },
+  {
+    "revision": "c2ee9ec6dfc34305120c",
+    "url": "/app-list/static/js/runtime~main.c0c5e0f5.js"
+  }
+]);
